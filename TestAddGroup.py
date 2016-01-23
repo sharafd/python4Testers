abe032806c2f799dc7dcc2f5e0db3c14a8acec88
Damir Sharafetdinov <sharafetdinoff@gmail.com>
2016-01-23 13:28:46 +0400

    * Выделение параметров

--- TestAddGroup.py
+++ TestAddGroup.py
@@ -26,28 +26,28 @@ class TestAddGroup(unittest.TestCase):
         wd.get("http://192.168.1.25/addressbook/index.php")
 
     # Логин
-    def login(self, wd):
+    def login(self, wd, login="admin", password="secret"):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(login)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     # Создание новой группы контактов
-    def add_new_contacts_group(self, wd):
+    def add_new_contacts_group(self, wd, group_name="New_01", group_header="+", group_footer="------------"):
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("New_01")
+        wd.find_element_by_name("group_name").send_keys(group_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("+")
+        wd.find_element_by_name("group_header").send_keys(group_header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("------------")
+        wd.find_element_by_name("group_footer").send_keys(group_footer)
         wd.find_element_by_name("submit").click()
 
     def logout(self, wd):
